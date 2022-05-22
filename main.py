9c563ab27fd030aa865ac8d09ea9eafbb65ac96c
Nicholas Domanico <3966361+Nicholas4096@users.noreply.github.com>
2022-05-22 00:29:22 -0400

    Added comments, typing, cleaned up scanner methods

--- main.py
+++ main.py
@@ -7,18 +7,23 @@ from dns import resolver, reversename
 from subprocess import Popen, PIPE
 import typing
 
+# Parses commandline agruments
 parser = argparse.ArgumentParser()
 parser.add_argument("--target", "-t", type=str, required=True)
 parser.add_argument("--ports", "-p", type=str)
 parser.add_argument("--quick", "-q", action="store_true")
 
-#args = parser.parse_args()
+args = parser.parse_args()
 
+# Scanner class
+# All methods for scanning or detecting hosts are contained in this class
 class Scanner:
     def __init__(self):
         pass
 
-    def dns_lookup(self, ip_to_search):
+    # Performs a reverse DNS lookup of each IP address in ip_to_search
+    # Returns a dict containing IP:Hostname key value pair
+    def dns_lookup(self, ip_to_search: list) -> dict:
         names = []
         dns_resolver = resolver.Resolver()
         nameserver = dns_resolver.nameservers[0]
@@ -32,9 +37,12 @@ class Scanner:
                 names.append(str(ipaddress.IPv4Address(ip)))
                 names.append(str(ipaddress.IPv4Address(ip)))
                 continue
-        return names
+        names_dict = {names[i]: names[i + 1] for i in range(0, len(names), 2)}
+        return names_dict
 
-    def ping_scan(self, ip_to_ping: list) -> list:
+    # Pings each IP address in ip_to_ping
+    # Returns a dict with host:state key value pairs
+    def ping_scan(self, ip_to_ping: list) -> dict:
         host_states = []
         commands = [["ping", "-n", "1", "-w", "200", str(ipaddress.IPv4Address(ip))] for ip in ip_to_ping]
         procs = [Popen(i, stderr=PIPE, stdout=PIPE) for i in commands]
@@ -43,20 +51,23 @@ class Scanner:
             host_states.append(str(ipaddress.IPv4Address(ip_to_ping[i])))
             host_states.append(p.returncode)
         states_dict = {host_states[i]: host_states[i + 1] for i in range(0, len(host_states), 2)}
-        print(states_dict)
+        return states_dict
 
+    # Will only detect hosts currently discoverable with ping
+    # Pings each IP address in ip_range, then performs a dns_lookup() of each host currently discoverable
+    # Returns list of IPs and hostnames
+    def quick_scan(self, ip_range: list) -> list:
+        up_hosts = []
+        ping_result = self.ping_scan(ip_range)
+        for ip in ping_result.keys():
+            if ping_result[ip] == 0:
+                up_hosts.append(ipaddress.IPv4Address(ip))
+            else:
+                continue
+        dns_result = self.dns_lookup(up_hosts)
+        names = [name for name in dns_result.values()]
 
-
-# Testing
-c = Scanner()
-
-start_ip = ipaddress.IPv4Address("192.168.254.1")
-end_ip = ipaddress.IPv4Address("192.168.254.254")
-
-ips = [i for i in range(int(start_ip), int(end_ip + 1))]
-
-
-c.ping_scan(ips)
+        return up_hosts
 
 
 
